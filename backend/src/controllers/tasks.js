b6ab637185d12377da921778fb7f4853ee57a252
David2508 <deividrocan2508@gmail.com>
2022-06-16 23:48:27 -0500

    add: BD connection

--- backend/src/controllers/tasks.js
+++ backend/src/controllers/tasks.js
@@ -1,3 +1,28 @@
-export const getTasks = (req, res) => {
+import {connect} from '../database'
+
+export const getTasks = async (req, res) => {
+  const db = await connect();
+ const [rows] = await db.query('SELECT * FROM tasks')
+  console.log(rows)
+ res.send('hello wordl');
+}
+
+export const getTask = (req, res) => {
   res.send('hello wordl');
-    }
+}
+
+export const getTaskCount= (req, res) => {
+  res.send('hello wordl');
+}
+
+export const saveTask = (req, res) => {
+  res.send('hello wordl');
+}
+
+export const deleteTask = (req, res) => {
+  res.send('hello wordl');
+}
+
+export const updateTask = (req, res) => {
+  res.send('hello wordl');
+}
